6b9bc27f1bcf286f0e308a589d9f00c608a144e4
Sangwa-C <chelseasabinesangwa@gmail.com>
2019-10-12 17:26:54 +0200

    activate intent

--- app/src/main/java/com/example/ihahire/MainActivity.java
+++ app/src/main/java/com/example/ihahire/MainActivity.java
@@ -2,13 +2,38 @@ package com.example.ihahire;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    private  Button mBuying;
+    private Button  mSelling;
+
+    public MainActivity() {
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mBuying=(Button).findById(R.id.buying);
+        mBuying.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public  void onClick(View b){
+                Intent intent= new Intent(MainActivity.this,Buy.class);
+                startActivity(intent);
+            }
+        });
+        mSelling=(Button).findById(R.id.selling);
+        mSelling.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View s) {
+                Intent intent= new Intent(MainActivity.this,sell.class);
+                startActivity(intent);
+            }
+        });
     }
 }
