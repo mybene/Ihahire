ed86fdf35b03b9f071bcd83ce7d186898dd73388
Sangwa-C <chelseasabinesangwa@gmail.com>
2019-10-13 19:48:38 +0200

    add images on choice_activity.xml

--- app/src/main/java/com/example/ihahire/choice.java
+++ app/src/main/java/com/example/ihahire/choice.java
@@ -2,9 +2,14 @@ package com.example.ihahire;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.Image;
 import android.os.Bundle;
 
 public class choice extends AppCompatActivity {
+
+    private Image mBuy;
+    private  Image mSell;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
