abb94d1d11f9f5ae5643b87c95a93255e7a0a331
Sangwa-C <chelseasabinesangwa@gmail.com>
2019-10-13 20:24:59 +0200

    intent the buy activity from choices activity

--- app/src/main/java/com/example/ihahire/buy.java
+++ app/src/main/java/com/example/ihahire/buy.java
@@ -2,13 +2,38 @@ package com.example.ihahire;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
 
 public class buy extends AppCompatActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_buy);
+//    }
+private TextView mBuyList;
+    private ListView mProductList;
+    private String[] products = new String[]{"Cabbage", "carrot","","brocoli", "persil", "eegplant", "coniflower", "black bean", "peas", "lentil", "potatoes", "pumpinks",
+            "sweet potatoes", "garlic", "basil", "coriander", "parsely", "lettuce", "peppers", "tomatoes"};
+
+    @SuppressLint("WrongViewCast")
+    protected void onCreate(Bundle savedInstancestate, Object adapterView, String name) {
+        super.onCreate(savedInstancestate);
         setContentView(R.layout.activity_buy);
+        mProductList = (ListView) findViewById(R.id.productList);
+        mBuyList = (TextView) findViewById(R.id.buyList);
+
+        ArrayAdapter adapter=new ArrayAdapter(this,android.R.layout.simple_list_item_1,products);
+        mProductList.setAdapter(adapter);
+
+        Intent intent=getIntent();
+
+        String buylist= intent.getStringExtra("name");
+        mBuyList.setText("Name of product:"+name);
     }
 }
--- app/src/main/java/com/example/ihahire/choice.java
+++ app/src/main/java/com/example/ihahire/choice.java
@@ -2,18 +2,47 @@ package com.example.ihahire;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.media.Image;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class choice extends AppCompatActivity {
 
-    private Image mBuy;
-    private  Image mSell;
+    private Button mBuy;
+    private Button mSell;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_choice);
+
+        mBuy=(Button)findViewById(R.id.buy);
+
+
+     mBuy.setOnClickListener(new View.OnClickListener() {
+
+           @Override
+           public void onClick(View v) {
+               Intent intent = new Intent(choice.this, buy.class);
+               startActivity(intent);
+
+           }
+       });
+
+        mSell=(Button)findViewById(R.id.sell);
+        mSell.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent1=new Intent(choice.this,Sell.class);
+                startActivity(intent1);
+            }
+        });
+
+
+
+
     }
 }
