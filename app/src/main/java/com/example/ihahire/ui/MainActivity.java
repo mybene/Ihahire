1af4c57491b900df623f5a3b8e6fd4b9533eca5c
Sangwa-C <chelseasabinesangwa@gmail.com>
2019-10-25 21:06:33 +0200

    Update the recyclerview dependecies

--- app/src/main/java/com/example/ihahire/ui/MainActivity.java
+++ app/src/main/java/com/example/ihahire/ui/MainActivity.java
@@ -3,14 +3,22 @@ package com.example.ihahire.ui;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 import com.example.ihahire.R;
 
 public class MainActivity extends AppCompatActivity {
 
+    private TextView WelcomeTextView;
+    private Button Start;
+    
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
     }
 }
