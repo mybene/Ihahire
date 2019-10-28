ce0d5441d74ed096b457728f29950023a526bcb4
Sangwa-C <chelseasabinesangwa@gmail.com>
2019-10-28 10:40:42 +0200

    update loginActivty

--- app/src/main/java/com/example/ihahire/ui/LoginActivity.java
+++ app/src/main/java/com/example/ihahire/ui/LoginActivity.java
@@ -1,16 +1,35 @@
 package com.example.ihahire.ui;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.ihahire.R;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 public class LoginActivity extends AppCompatActivity {
+    @BindView(R.id.createAccount) TextView registartion;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        ButterKnife.bind(this);
+
+        registartion.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View createAccount) {
+                Intent newAccount=new Intent(LoginActivity.this,SignUpActivity.class);
+                startActivity(newAccount);
+               finish();
+            }
+        });
     }
 }
--- app/src/main/java/com/example/ihahire/ui/MainActivity.java
+++ app/src/main/java/com/example/ihahire/ui/MainActivity.java
@@ -1,17 +1,21 @@
 package com.example.ihahire.ui;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
+import androidx.appcompat.app.AppCompatActivity;
+
 import com.example.ihahire.R;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 public class MainActivity extends AppCompatActivity {
-    private  Button mVasy;
+    @BindView(R.id.above)Button login ;
+
 
 
 
@@ -20,17 +24,17 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mVasy=(Button)findViewById(R.id.vasy);
-        mVasy.setOnClickListener(new View.OnClickListener(){
+        ButterKnife.bind(this);
+
+        login.setOnClickListener(new View.OnClickListener(){
             @Override
             public  void onClick(View v) {
-                //Toast to show the user that  buying  choiceActivity is loading
+                //Toast to show the user that  buying  login page is loading
                 Toast.makeText(MainActivity.this, "Welcome!!!", Toast.LENGTH_LONG).show();
 
-                Intent intent= new Intent(MainActivity.this, choiceActivity.class);
+                Intent intent= new Intent(MainActivity.this, LoginActivity.class);
                 startActivity(intent);
             }
-
 
         });
 
